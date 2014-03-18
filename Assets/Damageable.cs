de7a551c7c7c50554878981bcd5c4fd8ea5dccab
Nick Benson <nickjbenson@gmail.com>
2014-03-18 16:30:35 -0400

    Basic platforming Unity project from another project of mine

---
+++ Assets/Damageable.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Damageable : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void Damage() {
+		Debug.Log (this.gameObject + " damaged, destroying");
+		Destroy (this.gameObject);
+	}
+}
---
+++ Assets/Spawner.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour {
+
+	// Spawn timer in seconds
+	public float spawnTime = 1f;
+	private float spawnTimer = 0f;
+	public GameObject thingToSpawn;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		spawnTimer += Time.deltaTime;
+
+		if (spawnTimer >= spawnTime) {
+			spawnTimer = 0f;
+			Spawn();
+		}
+	
+	}
+
+	void Spawn() {
+		Object.Instantiate (thingToSpawn, this.transform.position, this.transform.rotation);
+	}
+}

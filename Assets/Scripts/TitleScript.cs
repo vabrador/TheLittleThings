d6c8e36698c4cc617048dd0b21c2a12bdb3b737f
Royal Morris <royal@mit.edu>
2014-04-24 13:35:07 -0400

    This is a really really basic Start screen. I can make this prettier, but it looks like a start screen and it should work as long as the correct name of the first scene is given as input to the load method. I will add comments soon.

---
+++ Assets/Scripts/TitleScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TitleScript : MonoBehaviour {
+	
+	public GUISkin skin;
+	public float halfScreenWidth = Screen.width / 2;
+	public float halfScreenHeight = Screen.height / 2;
+	public float buttonWidth = 320;
+	public float buttonHeight = 200;
+	
+	public void OnGUI() {
+		GUI.skin = skin;
+		float buttonX = halfScreenWidth - 160;
+		float buttonY = halfScreenHeight * 6 / 10;
+		if (GUI.Button(new Rect(buttonX, buttonY, buttonWidth, buttonHeight), "Start Game")) {
+			Application.LoadLevel("The Beginning");
+		}
+	}
+	
+}

d8c794cf6b0b00236933a4f4748db6bc6b76d763
John O'Sullivan <johno@mit.edu>
2014-05-07 22:32:55 -0400

    Fixed collisions with new animations!  Moving on to the next big features now.

--- Assets/Scripts/P1_controls.cs
+++ Assets/Scripts/P1_controls.cs
@@ -5,14 +5,12 @@ public class P1_controls : MonoBehaviour {
 	MovementAnimationScript mover;
 	// Use this for initialization
 	void Start () {
-		GameObject thisGuy = gameObject;
 		mover = gameObject.GetComponent<MovementAnimationScript>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if ((Input.GetAxis("P1 - Dash") > 0) && (!mover.stateBools["animating"])) {
-			Debug.Log ("Linda should dash now.");
 			mover.Dash ();
 		}
 		else if ((Input.GetAxis("P1 - Block") > 0)  && (!mover.stateBools["animating"])) {
